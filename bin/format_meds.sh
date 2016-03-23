e6ee01026599b2be9d78ba0f3a82d7a48bb58ad7
Brian Schrader <brian@brianschrader.com>
2016-03-23 14:34:25 -0700

    Filters meds by L-Dopa and removes tilt maze entries

--- bin/format_meds.sh
+++ bin/format_meds.sh
@@ -4,4 +4,10 @@
 # Usage: ./<script> <input>
 INPUT=$1
 
-cat $INPUT | cut -d ',' -f 1,2 | sed '1d' | sed 's/\([a-zA-Z ]\)=.*/\1/g'  
+cat $INPUT | cut -d ',' -f 1,2 | 
+    # Remove the first line and grab the medication name
+    sed '1d' | sed 's/\([a-zA-Z ]\)=.*/\1/g' | 
+    # Filter everything that's not a tilt-maze entry.
+    grep -iv 'maze' |
+    # Optional: Only grab L-Dopa meds.
+    grep -i 'dopa'
